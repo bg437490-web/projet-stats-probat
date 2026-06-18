6857896ce2700a6264f96b855c46ade55837f090
boyeazizsamb1010-ai <boyeazizsamb1010@gmail.com>
2026-06-18 15:52:02 +0000

    feat: Q2 dénombrement façons de choisir 3 boissons

--- abdou/denombrement.py
+++ abdou/denombrement.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,3 +10,8 @@ nb_boissons = df['boisson'].nunique()
 nb_moments = df['moment_journee'].nunique()
 combinaisons = nb_boissons * nb_moments
 print(f"Combinaisons possibles de (boisson × moment de la journée) : {combinaisons}")
+
+# Combien de façons différentes un étudiant peut consommer 3 boissons dans une journée ?
+nb_boissons = df['boisson'].nunique()
+nbre_facon_de_boissons = math.comb(nb_boissons, 3)
+print(f"Façons de choisir 3 boissons : {nbre_facon_de_boissons}")
